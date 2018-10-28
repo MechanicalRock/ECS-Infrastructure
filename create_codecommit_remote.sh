ba67faf306c2b7beea4df5473fb8b05899c5fc98
Joshua Armitage <josh.armitage@outlook.com>
2018-10-28 11:01:48 +0800

    Added Scripting Around Deployment
    In order to simplify the usage of the repository there are now scripts
    that allow for easier updating of the pipeline and the DNS
    infrastructure.
    
    Documentation to come in the following commit.

---
+++ create_codecommit_remote.sh
@@ -0,0 +1,7 @@
+set -e
+
+CLONE_URL=$(aws cloudformation describe-stacks --stack-name ecsInfrastructurePipeline --profile $2 | grep git-codecommit | awk '{ print $2 }' | sed s/\"//g)
+git remote add $1 ${CLONE_URL}
+
+git config --local credential.helper '!aws --profile $2 codecommit credential-helper $@'
+git config --local credential.UseHttpPath true
---
+++ deploy.sh
@@ -0,0 +1,24 @@
+set -e
+
+STACKS=$(aws cloudformation list-stacks --profile $2)
+if [[ $(echo ${STACKS}) == *route53Infrastructure* ]]; then
+  aws cloudformation create-change-set --stack-name route53Infrastructure --change-set-name dns-change-set --change-set-type UPDATE --template-body file://dns.yml --parameters file://$1.json --profile $2
+  if [[ $(aws cloudformation describe-change-set --stack-name route53Infrastructure --change-set-name dns-change-set --profile $2 | grep "Changes") != *[]* ]]; then
+    aws cloudformation update-stack --stack-name route53Infrastructure --template-body file://dns.yml --parameters file://$1.json --profile $2
+    aws cloudformation wait stack-update-complete --stack-name route53Infrastructure --profile $2
+  fi
+else
+  aws cloudformation create-stack --stack-name route53Infrastructure --template-body file://dns.yml --parameters file://$1.json --profile $2
+  aws cloudformation wait stack-create-complete --stack-name route53Infrastructure --profile $2
+fi
+
+if [[ $(echo ${STACKS}) == *ecsInfrastructurePipeline* ]]; then
+  aws cloudformation create-change-set --stack-name ecsInfrastructurePipeline --change-set-name pipeline-change-set --change-set-type UPDATE --template-body file://pipeline.yml --parameters file://$1.json --capabilities CAPABILITY_NAMED_IAM  --profile $2
+  if [[ $(aws cloudformation describe-change-set --stack-name ecsInfrastructurePipeline --change-set-name pipeline-change-set --profile $2 | grep "Changes") != *[]* ]]; then
+    aws cloudformation update-stack --stack-name ecsInfrastructurePipeline --template-body file://pipeline.yml --parameters file://$1.json --capabilities CAPABILITY_NAMED_IAM --profile $2
+    aws cloudformation wait stack-update-complete --stack-name ecsInfrastructurePipeline --profile $2
+  fi
+else
+  aws cloudformation create-stack --stack-name ecsInfrastructurePipeline --template-body file://pipeline.yml --parameters file://$1.json --capabilities CAPABILITY_NAMED_IAM --profile $2
+  aws cloudformation wait stack-create-complete --stack-name ecsInfrastructurePipeline --profile $2
+fi

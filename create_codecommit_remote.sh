1555ddc79e1bb7d7980fb412c4a044eb6e895046
Joshua Armitage <josh.armitage@outlook.com>
2018-10-28 14:22:57 +0800

    Updated parameters for test account

--- create_codecommit_remote.sh
+++ create_codecommit_remote.sh
@@ -1,7 +1,4 @@
-set -xe
+set -e
 
 CLONE_URL=$(aws cloudformation describe-stacks --stack-name ecsInfrastructurePipeline --profile $2 | grep git-codecommit | awk '{ print $2 }' | sed s/\"//g)
 git remote add $1 ${CLONE_URL}
-
-git config --local credential.helper $(echo '!aws --profile $2 codecommit credential-helper $@' | sed s/\$2/$2/)
-git config --local credential.UseHttpPath true
